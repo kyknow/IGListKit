65e0f480d2e056d13d454ce8cc9fc30ce738a5d8
phil zhang <goo.gle@foxmail.com>
2021-08-21 11:47:56 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,17 @@ import PackageDescription
 let package = Package(
     name: "IGListKit",
     products: [
-        .library(name: "IGListKit", targets: ["IGListKit"])
+        .library(name: "IGListKit", targets: ["IGListKit"]),
+        .library(name: "IGListDiffKit", targets: ["IGListDiffKit"])
     ],
     targets: [
         .target(
             name: "IGListKit",
-            path: "Source"
+            path: "Source/IGListKit"
+        ),
+        .target(
+            name: "IGListDiffKit",
+            path: "Source/IGListDiffKit"
         )
     ]
 )
